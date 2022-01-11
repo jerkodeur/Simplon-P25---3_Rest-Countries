6a3f6ecd497a32cb0e5c1200a7e8762f7b46d3da
jerkoLegion <jerome.potie@gmail.com>
2022-01-11 11:50:48 +0100

    Improve the fetch call to save some ressources

---
+++ index.js
@@ -0,0 +1,14 @@
+const render = async () => {
+  const response = await fetch(
+    "https://restcountries.com/v3/region/europe?fields=name"
+  ).then((datas) => datas.json());
+
+  let nameOfCountries = "";
+  response.forEach((countryName) => {
+    nameOfCountries += `<li>${countryName.name.official}</li>`;
+  });
+
+  document.querySelector("#countries").innerHTML = nameOfCountries;
+};
+
+window.addEventListener("load", render());
