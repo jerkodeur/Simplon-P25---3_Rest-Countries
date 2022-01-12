54f9e4683a5bb2fa6c963b4a2c3c6889a96e316d
jerkoLegion <jerome.potie@gmail.com>
2022-01-12 22:26:18 +0100

    Change geolocation method

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,27 +1,34 @@
 const render = async () => {
-  const response = await fetch(
-    "https://restcountries.com/v3.1/region/europe?fields=name,capitalInfo"
-  ).then((datas) => datas.json());
+  const response = await request(
+    "https://restcountries.com/v3.1/region/europe?fields=name,cca2"
+  );
 
-  const nameOfCountries = response.reduce((acc, curr, index) => {
+  const nameOfCountries = await response.reduce((acc, curr, index) => {
     const selected = curr.name.common === "France" ? "selected" : "";
-    const [lat, long] = curr.capitalInfo.latlng;
-
-    acc += `<option ${selected} data-lat=${lat} data-long=${long} value=${index}>${curr.name.official}</option>`;
+    acc += `<option ${selected} data-code=${curr.cca2} value=${index}>${curr.name.official}</option>`;
     return acc;
   }, "");
 
-  document.querySelector("#countryList").innerHTML = nameOfCountries;
+  document.querySelector("#countryList").innerHTML = await nameOfCountries;
 
   updateMap();
 };
 
-const updateMap = () => {
+const request = (endpoint) => fetch(endpoint).then((datas) => datas.json());
+
+const updateMap = async () => {
   const countryList = document.querySelector("#countryList");
-  const { lat, long } = countryList.options[countryList.value].dataset;
+  const code = countryList.options[countryList.value].dataset.code;
+
+  const response = await request(
+    `https://restcountries.com/v3.1/alpha?codes=${code}&fields=capitalInfo`
+  );
+
+  const [lat, long] = await response[0].capitalInfo.latlng;
 
   const map = document.querySelector("#map");
-  map.src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyBgv-jGQXDVSbmK_NS0K5_XeJy4RoGzX9M&center=${lat},${long}&zoom=10&language=en`;
+  map.src =
+    await `https://www.google.com/maps/embed/v1/view?key=AIzaSyBgv-jGQXDVSbmK_NS0K5_XeJy4RoGzX9M&center=${lat},${long}&zoom=10&language=en`;
 };
 
 window.addEventListener("load", render);
