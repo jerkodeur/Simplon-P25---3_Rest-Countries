e68e31ed3edff07aab4e470b8b86ec2c4aa09f63
jerkoLegion <jerome.potie@gmail.com>
2022-01-13 20:26:18 +0100

    Fix issues #1

--- js/geolocation.js
+++ js/geolocation.js
@@ -1,34 +1,35 @@
 const render = async () => {
-  const response = await request(
+  const europeeanCountries = await getApiData(
     "https://restcountries.com/v3.1/region/europe?fields=name,cca2"
   );
 
-  const nameOfCountries = await response.reduce((acc, curr, index) => {
-    const selected = curr.name.common === "France" ? "selected" : "";
-    acc += `<option ${selected} data-code=${curr.cca2} value=${index}>${curr.name.official}</option>`;
-    return acc;
-  }, "");
+  let nameOfCountries = "";
 
-  document.querySelector("#countryList").innerHTML = await nameOfCountries;
+  europeeanCountries.forEach((country) => {
+    nameOfCountries += `<option value="${country.cca2}">${country.name.official}</option>`;
+  });
+
+  const el = document.querySelector("#countryList");
+  el.innerHTML = nameOfCountries;
+  el.value = "FR";
 
   updateMap();
 };
 
-const request = (endpoint) => fetch(endpoint).then((datas) => datas.json());
+const getApiData = (endpoint) => fetch(endpoint).then((datas) => datas.json());
 
 const updateMap = async () => {
-  const countryList = document.querySelector("#countryList");
-  const code = countryList.options[countryList.value].dataset.code;
+  const ISO2Code = document.querySelector("#countryList").value;
 
-  const response = await request(
-    `https://restcountries.com/v3.1/alpha?codes=${code}&fields=capitalInfo`
+  const capitalGeoLocation = await getApiData(
+    `https://restcountries.com/v3.1/alpha?codes=${ISO2Code}&fields=capitalInfo`
   );
 
-  const [lat, long] = await response[0].capitalInfo.latlng;
+  const [lat, long] = capitalGeoLocation[0].capitalInfo.latlng;
 
-  const map = document.querySelector("#map");
-  map.src =
-    await `https://www.google.com/maps/embed/v1/view?key=AIzaSyBgv-jGQXDVSbmK_NS0K5_XeJy4RoGzX9M&center=${lat},${long}&zoom=10&language=en`;
+  document.querySelector(
+    "#map"
+  ).src = `https://www.google.com/maps/embed/v1/view?key=AIzaSyBgv-jGQXDVSbmK_NS0K5_XeJy4RoGzX9M&center=${lat},${long}&zoom=10&language=en`;
 };
 
 window.addEventListener("load", render);
